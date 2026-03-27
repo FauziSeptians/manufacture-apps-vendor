c743d0f65002da7fa1e438f26a33e209829821b1
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2026-03-27 14:09:45 +0700

    fix: seo

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://wartiwan-industri.vercel.app/sitemap.xml',
+    sitemap: 'https://www.wartiwanindustri.com/sitemap.xml',
   };
 }
